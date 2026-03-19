875f83baa4b4414f34f95496bb3666215cc7509f
iveiks <iveiks@gmail.com>
2026-03-19 10:33:28 +0200

    uuendus

--- autorent/single_car.php
+++ autorent/single_car.php
@@ -1,51 +1,9 @@
 <?php include('config.php'); ?>
-
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Autorent</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  </head>
-  <body>
-<!-- menüü -->
-    <nav class="navbar navbar-expand-lg bg-body-tertiary mb-4">
-  <div class="container">
-    <a class="navbar-brand" href="#">Autorent</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Avaleht</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Autod</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Hinnad</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Kontakt</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search" method="get" action="index.php">
-        <input class="form-control me-2" type="search" placeholder="Otsi" aria-label="Search" name="otsi">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-<!-- menüü -->
+<?php include('header.php'); ?>
 
 <!-- sisu -->
 <div class="container">
     <div class="row">
-
-        
 
         <?php
         $id = $_GET['id'];
@@ -58,13 +16,19 @@
             <h1><?php echo $rida["mark"]; ?><?php echo $rida["model"]; ?></h1>
             <p>Mootor: <?php echo $rida["engine"]; ?></p>
             <p>Kütus: <?php echo $rida["fuel"]; ?></p>
-            <p>Hind: <?php echo $rida["price"]; ?></p>
+            <p>Aasta: <?php echo $rida["year"]; ?></p>
+            <p>Staatus: <?php echo $rida["status"]; ?></p>
+            <p>Käigukast: <?php echo $rida["transmission"]; ?></p>
+            <p>Istmed: <?php echo $rida["seats"]; ?></p>
+            <p class="fs-5">Hind: <?php echo $rida["price"]; ?></p>
+            <a class="btn btn-dark" href="index.php">Tagasi</a>
+            <a href="#" class="btn btn-dark w-100">Rendi</a>
         </div>
         <div class="col">
             <img src="https://loremflickr.com/800/500/<?php echo str_replace(" ","", $rida["mark"]); ?>" class="card-img-top img-fluid" alt="<?php echo str_replace(" ","", $rida["mark"]); ?>">
         </div>
     </div>
-    <a href="index.php">Tagasi</a>
+    
 </div>
 <!-- /sisu -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
